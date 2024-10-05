7532ec6534ec97abe1b44b6d5d260c1fa8edae4d
Ole <ole.reimers@gmx.de>
2024-10-06 01:20:52 +0200

    added shop

--- server/server.js
+++ server/server.js
@@ -7,15 +7,27 @@ const https = require('https');
 const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
+const nodemailer = require("nodemailer");
+const pug = require('pug');
 
 process.env.TZ = 'Europe/Berlin' 
-
+let password = null;
+let compileOrderTemplate = null;
+let compilePresentTemplate = null;
 let credentials = null;
+
 try {
   const privateKey = fs.readFileSync('/var/opt/ssl/ole-reimers.key');
   const certificate = fs.readFileSync('/var/opt/ssl/ole-reimers.crt');
   credentials = {key: privateKey, cert: certificate};
 } catch (e) {}
+
+try {
+  password = fs.readFileSync('/var/opt/mail/password.key');
+  //password = fs.readFileSync('C:\\var\\opt\\mail\\password.key');
+  compileOrderTemplate = pug.compileFile(path.join(__dirname, 'templates', 'order.pug'));
+  compilePresentTemplate = pug.compileFile(path.join(__dirname, 'templates', 'present.pug'));
+} catch(e) {}
 
 const PORT = process.env.PORT || 80;
 const app = express();
@@ -84,6 +96,87 @@ for (let releaseDateIndex = 0; releaseDateIndex < releaseDates.length; releaseDa
     }
   });
 }
+
+// mails
+
+function validateEmail(email) {
+  if (typeof email != "string") return false;
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+app.post('/order', (req, res) => {
+  if (!password) { res.sendStatus(500); return; }
+  if (!compileOrderTemplate) { res.sendStatus(500); return; }
+  if (!compilePresentTemplate) { res.sendStatus(500); return; }
+
+  data = req.body;
+
+  if (data.type !== "present" && data.type !== "order") {
+    res.status(400).json({
+      message: "Bitte verwende einen gültigen Auftragstypen"
+    });
+    return;
+  }
+  if (!validateEmail(data.email)) {
+    res.status(400).json({
+      message: "Bitte gib eine gültige E-Mail Adresse ein."
+    });
+    return;
+  }
+
+  if (typeof data.price != "number" || typeof data.amount != "number" || typeof data.name != "string" || typeof data.message != "string") {
+    res.status(400).json({
+      message: "Bitte gib gültige Werte ein."
+    });
+    return;
+  }
+
+  const transporter = nodemailer.createTransport({
+    port: 587,
+    host: "smtp.ionos.de",
+    auth: {
+      user: "shop@ole-reimers.de",
+      pass: password
+    }
+  });
+
+  let message = "";
+
+  if (data.type === "present") {
+    const mailHtml = compilePresentTemplate(data);
+
+    message = {
+      from: 'Ole Reimers Shop <shop@ole-reimers.de>',
+      to: data.email,
+      bcc: "ole.reimers@gmx.de",
+      subject: "Geschenkbestätigung Ole Reimers Shop",
+      html:  mailHtml
+    };
+  } else {
+    const mailHtml = compileOrderTemplate(data);
+
+    message = {
+      from: "Ole Reimers Shop <shop@ole-reimers.de>",
+      to: data.email,
+      bcc: "ole.reimers@gmx.de",
+      subject: "Bestellbestätigung Ole Reimers Shop",
+      html:  mailHtml
+    };
+  }
+  
+
+  transporter.verify(function (error, success) {
+    if (error) {
+      res.sendStatus(500);
+      return;
+    } else {
+      transporter.sendMail(message);
+      res.sendStatus(201);
+      return;
+    }
+  });
+});
 
 // angular
 app.get('*', (req, res) => {
